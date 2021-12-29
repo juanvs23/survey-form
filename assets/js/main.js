4824d2e34ff2526e0f1e3255ae5ecd4542fe9945
Juan Carlos Avila <juan.avila@nivelics.co>
2021-12-28 22:00:11 -0400

    upload to github

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const year = new Date();
+const form = document.getElementById("survey-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+document.addEventListener("DOMContentLoaded", () => {});
